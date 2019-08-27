3063737f163723ee6c1d30f2d2240903ca15152e
Alex <alex.step101@gmail.com>
2019-08-27 12:13:38 -0400

    Init Commit

---
+++ src/app/shared/models/models.ts
@@ -0,0 +1,86 @@
+//NOT USED
+//
+// export class PrintData {
+//     UserId: number;
+//     id: string;
+//     name: string;
+//     orderId: string;  
+//     desingId: string;   
+//     status: string;
+//     fontFamily: string;
+//     fontStyle: string;
+//     fontWeight: string;
+//     fontColor: string;
+//     bottleSize: string;
+//     orderSize: string;
+//     warehouse: string;
+//     description: string;
+//     fontSize: number;
+//     orientation: number;
+//     x: number;
+//     y: number;
+//     height: number;
+//     width: number;
+// }
+
+// export class PaperColor {
+//     name: string;
+//     hexColor: string;
+
+//     constructor(colorName: string = "", colorCode: string = "") {
+//           this.name = colorName;
+//           this.hexColor = colorCode;
+//     }
+// }
+
+// export class Point {
+//    x: number;
+//    y: number;
+
+//    constructor(x: number = 0, y: number = 0) {
+//        this.x = x;
+//        this.y = y;
+//    }
+// }
+
+// export class Size {
+//    width: number;
+//    height: number;
+
+//    constructor(width: number, height: number) {
+//        this.width = width;
+//        this.height = height;
+//    }
+// }
+
+// export class Rectangle {
+//     id: string;
+//     canvasId: string;
+//     groupId: string;
+//     x: number;
+//     y: number;
+//     title: string;
+//     text: string;
+//     measure: string;
+//     zoom: number;
+//     width: number;
+//     height: number;
+//     bgColor: string;
+//     borderColor: string;
+//     borderWidth: number;
+//     showDetails: boolean;
+//     imageUrl: string;
+//     imageLoaded: boolean;
+//     imageData: PrintData;
+//     titleData: PrintData;
+//     texteData: PrintData;
+//     waftWord: PrintData;
+//     printCode: PrintData;    
+
+//     constructor(x: number = 0, y: number = 0, width: number = 0, height: number = 0) {
+//           this.x = x;
+//           this.y = y;
+//           this.width = width;
+//           this.height = height;
+//     }
+// }
